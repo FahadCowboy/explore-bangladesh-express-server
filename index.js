acdc74d4f24227ab3caeaa5d6f634199aaa85e8f
FahadCowboy <olivierafahad@gmail.com>
2021-10-29 14:57:08 +0600

    Get API is created for all the places to visit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 4000
 const ObjectId = require('mongodb').ObjectId
+const { query } = require('express')
 
 
 // testDB
@@ -17,10 +18,28 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.z46cs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z46cs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const run = async () => {
+   try{
+      await client.connect();
+      const database = client.db("exploreBangladesh")
+      const placesCollection = database.collection("places")
 
+      app.get('/places', async (req, res) => {
+         const query = {}
+         const cursor = placesCollection.find(query)
+         const places = await cursor.toArray()
+         res.send(places)
+      })
+
+   } finally {
+
+   }
+}
+
+run().catch(console.dir)
 
 
 
@@ -33,7 +52,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 app.get('/', (req, res) => {
-   console.log(uri)
+   // console.log(uri)
    res.send("server is running well!")
 })
 
